c2ab38b1803c7576fd3b1156811da0f02c646746
dhruvi1930 <112989118+dhruvi1930@users.noreply.github.com>
2022-09-29 13:36:24 -0400

    connection of database

---
+++ connection.php
@@ -0,0 +1,13 @@
+<?php
+$servername = "localhost";
+$username = "root";
+$password = "";
+$db = "dp";
+// Create connection
+$conn = mysqli_connect($servername, $username, $password,$db);
+// Check connection
+if (!$conn) {
+   die("Connection failed: " . mysqli_connect_error());
+}
+
+?>
